6fc4acbf45528f133576f829f3183138e5a92a35
Michaela Guzmická <mgmichaelaguzmicka@gmail.com>
2021-02-13 22:37:45 +0100

    initiate commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const bcrypt = require("bcrypt");
+
+app.use(express.json());
+
+const users = [];
+
+// create a route
+// GET request
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// POST request
+app.post("/users", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashed = await bcrypt.hash(req.body.password, salt);
+    const user = { name: req.body.name, password: hashed };
+    users.push(user);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.listen(3000);
